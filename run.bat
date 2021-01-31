8c77b911126a71668674bc9f302544de49e52c45
Daniel Popiniuc <danielpopiniuc@gmail.com>
2021-01-31 20:00:36 +0200

    minor enhancement of the variable handling

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
-@echo off
-SETLOCAL ENABLEEXTENSIONS
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET variable__parent=%~dp0
 CALL %variable__parent%_functions-Apps.bat :InitiateGlobalVariablesForInstallation
 CALL %variable__parent%_functions-Apps.bat :Menu__InstallationsToDo
 ENDLOCAL
+ECHO ON
